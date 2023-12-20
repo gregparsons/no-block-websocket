b81d9e18f4b4c142a89f1a51aa18b7cd25c49265
gp <github@swimr.com>
2023-12-20 12:55:59 -0800

    server use accept instead of stream iterator

--- src/server.rs
+++ src/server.rs
@@ -6,86 +6,94 @@ use std::sync::{Arc, Mutex};
 use std::thread::spawn;
 use std::time::Duration;
 
-use tungstenite::{accept_hdr, handshake::server::{Request, Response}, Message, WebSocket};
+use tungstenite::{Message, WebSocket};
 use tungstenite::protocol::CloseFrame;
 use tungstenite::protocol::frame::coding::CloseCode;
 
 #[derive(Debug)]
-pub struct Server{
-    // socket_arc_opt:Arc<Mutex<Option<WebSocket<TcpStream>>>>,
-}
-
+pub struct Server;
 impl Server {
     pub fn run() {
+        let tcp_listener = TcpListener::bind("127.0.0.1:3012").unwrap();
 
-        let server = TcpListener::bind("127.0.0.1:3012").unwrap();
-        for stream in server.incoming() {
-            spawn(move || {
-                let callback = |req: &Request, mut response: Response| {
-                    println!("Received a new ws handshake");
-                    println!("The request's path is: {}", req.uri().path());
-                    println!("The request's headers are:");
-                    for (ref header, _value) in req.headers() {
-                        println!("* {}", header);
-                    }
+        // for stream in listener.incoming()
 
-                    // Let's add an additional header to our response to the client.
-                    let headers = response.headers_mut();
-                    headers.append("MyCustomHeader", ":)".parse().unwrap());
-                    headers.append("SOME_TUNGSTENITE_HEADER", "header_value".parse().unwrap());
+        match tcp_listener.accept() {
+            Ok((tcp_stream, _addr)) => {
+                match tungstenite::accept(tcp_stream){
+                    Ok(ws) => {
 
-                    Ok(response)
-                };
-                let ws = accept_hdr(stream.unwrap(), callback).unwrap();
+                        spawn(move || {
 
-                let ws_arc:Arc<Mutex<WebSocket<TcpStream>>> = Arc::new(Mutex::new(ws));
+                            // let callback = |req: &Request, mut response: Response| {
+                                tracing::info!("[server] Received a new ws handshake");
+                                // println!("The request's path is: {}", req.uri().path());
+                            //
+                            //     // println!("The request's headers are:");
+                            //     // for (ref header, _value) in req.headers() {
+                            //     //     println!("* {}", header);
+                            //     // }
+                            //     // Let's add an additional header to our response to the client.
+                            //     // let headers = response.headers_mut();
+                            //     // headers.append("MyCustomHeader", ":)".parse().unwrap());
+                            //     // headers.append("SOME_TUNGSTENITE_HEADER", "header_value".parse().unwrap());
+                            //
+                            //     Ok(response)
+                            // };
+                            // let ws = accept_hdr(stream.unwrap(), callback).unwrap();
 
+                            let ws_arc:Arc<Mutex<WebSocket<TcpStream>>> = Arc::new(Mutex::new(ws));
 
-                let ws1 = ws_arc.clone();
-                spawn(move ||{
-                    std::thread::sleep(Duration::from_secs(20));
-                    tracing::error!("[server] closing...");
-                    let mut ws1 = ws1.lock().unwrap();
-                    ws1.close(Some(CloseFrame{ code: CloseCode::Normal, reason: Default::default() })).unwrap();
+                            let ws1 = ws_arc.clone();
+                            spawn(move ||{
+                                std::thread::sleep(Duration::from_secs(20));
+                                tracing::error!("[server] closing...");
+                                let mut ws1 = ws1.lock().unwrap();
+                                ws1.close(Some(CloseFrame{ code: CloseCode::Normal, reason: Default::default() })).unwrap();
+                            });
 
-    
-                });
+                            let ws2 = ws_arc.clone();
+                            loop {
+                                let mut ws2 = ws2.lock().unwrap();
 
-                let ws2 = ws_arc.clone();
-                loop {
-
-                    let mut ws2 = ws2.lock().unwrap();
-
-                    if let Ok(msg) = ws2.read() {
-                        match msg {
-                            Message::Text(txt) => {
-                                tracing::info!("[server] received text: {}", &txt);
-                                match ws2.send(Message::Text(format!("server rcvd: {txt}"))) {
-                                    Ok(_) => {},
-                                    Err(e) => {
-                                        tracing::error!("[server] send after close: {e:?}");
-                                        // ws2.close(None).unwrap();
-                                        break;
+                                // read loop
+                                if let Ok(msg) = ws2.read() {
+                                    match msg {
+                                        Message::Text(txt) => {
+                                            tracing::info!("[server] received text: {}", &txt);
+                                            match ws2.send(Message::Text(format!("server rcvd: {txt}"))) {
+                                                Ok(_) => {},
+                                                Err(e) => {
+                                                    tracing::error!("[server] send after close: {e:?}");
+                                                    // ws2.close(None).unwrap();
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        Message::Ping(_ping) => {
+                                            // Vec<u8>
+                                            let _ = ws2.send(Message::Pong(vec![]));
+                                        },
+                                        // Message::Binary(Vec<u8>)=>{
+                                        //
+                                        // },
+                                        // Message::Pong(Vec<u8>)=>{
+                                        //
+                                        // },
+                                        // Message::Close(Option<CloseFrame<'static>>),
+                                        // Message::Frame(Frame),
+                                        _ => {}
                                     }
                                 }
                             }
-                            Message::Ping(_ping) => {
-                                // Vec<u8>
-                                let _ = ws2.send(Message::Pong(vec![]));
-                            },
-                            // Message::Binary(Vec<u8>)=>{
-                            //
-                            // },
-                            // Message::Pong(Vec<u8>)=>{
-                            //
-                            // },
-                            // Message::Close(Option<CloseFrame<'static>>),
-                            // Message::Frame(Frame),
-                            _ => {}
-                        }
+                        });
+
+
                     }
+                    Err(_) => {}
                 }
-            });
+            }
+            Err(_) => {}
         }
     }
 }
